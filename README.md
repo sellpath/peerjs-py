dd1aaffecfe609a122a868424e7dba911974f965
Mansu Kim <mkim@sellpath.ai>
2024-09-22 13:09:47 -0700

    add publish

--- README.md
+++ README.md
@@ -11,23 +11,6 @@ To install peerjs-py, use pip:
 pip install peerjs-py
 ```
 
-
-peerjs_py/
-├── src/
-│   ├── peerjs_python/
-│   │   ├── __init__.py
-│   │   ├── dataconnection/
-│   │   │   ├── __init__.py
-│   │   │   └── BufferedConnection/
-│   │   │       ├── __init__.py
-│   │   │       ├── BinaryPack.py
-│   │   │       └── binaryPackChunker.py
-│   │   ├── logger.py
-│   │   └── enums.py
-├── setup.py
-├── README.md
-└── requirements.txt (optional)
-
 ## Usage
 
 ### Connecting to a PeerJS Server
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class PyTest(TestCommand):
 setup(
     name="peerjs_py",
     version="0.1.0",
-    packages=find_packages(where="src/peerjs_py"),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
         "aiortc>=1.9.0,<2.0.0",
@@ -19,7 +19,6 @@ setup(
         "requests>=2.25.1,<3.0.0",
         # "websockets>=9.1,<10.0",
     ],
-    package_dir={"": "src"},
     author="Mansu Kim",
     author_email="mkim@sellpath",
     description="A Python implementation of PeerJS",
