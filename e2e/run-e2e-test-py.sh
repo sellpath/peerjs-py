f6ca9ec83672646bce6fc823a5d6a1f8522b2e9a
Mansu Kim <mkim@sellpath.ai>
2024-09-22 14:00:34 -0700

    test

--- e2e/run-e2e-test-py.sh
+++ e2e/run-e2e-test-py.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Set the PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)/..:$(pwd)/../src
+
+# Set the working directory to the script's location
+cd "$(dirname "$0")"
+
 
 # Store the current directory and move to the parent directory
 pushd .. > /dev/null
